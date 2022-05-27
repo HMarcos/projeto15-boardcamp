8a5c7840b58e2a282e91f20bc2f1093784bfd7c9
Marcos Henrique <marcosmarcone48@gmail.com>
2022-05-27 15:59:15 -0300

    Initial configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import { info } from "./logging/logging.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(info(`The application is running on port ${PORT}...`));
+});
